5bb4216484971d3902e1c8a4174a121c044f3377
tnuocca wen <benshalomalbert@gmail.com>
2024-05-08 01:30:24 +0530

    google-signin update

--- static/js/gjs.js
+++ static/js/gjs.js
@@ -1,5 +1,4 @@
 function handleCredentialResponse(response) {
-  console.log("Encoded JWT ID token: " + response.credential);
   formData = new FormData();
   formData.append('jwt', response.credential);
   fetch(authURL, {
